23f2ca4987c2f1a9de5b20acc56bd7d98c452d90
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-13 22:24:29 +0530

    Added getters from store

--- src/components/FavoriteValue.vue
+++ src/components/FavoriteValue.vue
@@ -1,13 +1,13 @@
 <template>
     <h2>{{ counter }}</h2>
+    <p>we do more here....</p>
 </template>
 
 <script>
 export default {
     computed: {
         counter() {
-            // return this.$store.state.counter;
-            return this.$store.getters.finalCounter;
+            return this.$store.getters.normalisedCounter;
         }
     }
 }
