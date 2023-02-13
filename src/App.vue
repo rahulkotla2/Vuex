979b6fa8c95415332b27a3d80a6111e81aef599d
rahulkotla2 <rahulkotla2@gmail.com>
2023-02-13 21:42:24 +0530

    Added new component and Acceessing the State from Store in Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <base-container title="Vuex">
-    <h2>{{ $store.state.counter }}</h2>
+    <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
   },
   methods: {
     addOne() {
-       this.$store.state.counter++;
+      this.$store.state.counter++;
     }
   }
 };
